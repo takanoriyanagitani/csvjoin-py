dfa3ee3670b5ebec1b3a2a343aa4e2dd4a78fc77
yanagitani <takanori.yanagitani@gmail.com>
2020-05-14 04:48:32 +0900

    more tests.

--- csvjoin.py
+++ csvjoin.py
@@ -13,10 +13,10 @@ def iter2list(e=tuple(), ig=itemgetter(1)):
   )
 
 def join_ii(i1=iter([]), i2=iter([]), k1=itemgetter(0), k2=itemgetter(0), fi=iter2list):
-  m1 = map(lambda e: (k1(e), e), i1)
-  m2 = map(lambda e: (k2(e), e), i2)
+  m1 = map(lambda e: ((k1(e), 1), e), i1)
+  m2 = map(lambda e: ((k2(e), 2), e), i2)
   m = heapq.merge(*(m1,m2))
-  g = itertools.groupby(m, itemgetter(0))
+  g = itertools.groupby(map(lambda t: (t[0][0], t), m), itemgetter(0))
   return map(lambda e: fi(e), g)
 
 def join_ff(f1=sys.stdin, f2=sys.stdin): return join_ii(
@@ -24,7 +24,19 @@ def join_ff(f1=sys.stdin, f2=sys.stdin): return join_ii(
   csv.reader(f2),
 )
 
-def join_dd(r1=sys.stdin, r2=sys.stdin, f1=None, f2=None): return join_ii(
+def join_dd(r1=sys.stdin, r2=sys.stdin, f1=None, f2=None, k1=itemgetter("key"), k2=itemgetter("key")): return join_ii(
   csv.DictReader(r1,fieldnames=f1),
   csv.DictReader(r2,fieldnames=f2),
+  k1,
+  k2,
 )
+
+def join_nf(n1="f1.csv", n2="f2.csv"):
+  with open(n1, mode="r", newline="") as f1:
+    with open(n2, mode="r", newline="") as f2:
+      return join_ff(f1, f2)
+
+def join_nd(n1="f1.csv", n2="f2.csv", f1=None, f2=None, k1=itemgetter("key"), k2=itemgetter("key")):
+  with open(n1, mode="r", newline="") as r1:
+    with open(n2, mode="r", newline="") as r2:
+      return join_dd(r1, r2, f1, f2, k1, k2)
--- csvjoin_test.py
+++ csvjoin_test.py
@@ -1,9 +1,62 @@
 import unittest
+import os
+import csv
 
 import csvjoin
 
 class iter2list(unittest.TestCase):
   def test_empty(self): self.assertEqual(("", []), csvjoin.iter2list(("", [])))
+
+class join_ii(unittest.TestCase):
+  def test_empty(self): self.assertEqual(None, next(csvjoin.join_ii([],[]), None))
+
+class join_ff(unittest.TestCase):
+  def test1(self):
+    os.makedirs("./.test/join_ff/test1", exist_ok=True)
+    with open("./.test/join_ff/test1/f1.csv", "w", newline="") as f1: csv.writer(f1).writerows([
+      ["amazon",    6, "cloud"],
+      ["apple",     5, "phone"],
+      ["facebook",  8, "sns"],
+      ["google",    6, "search"],
+      ["microsoft", 9, "os"],
+    ])
+    with open("./.test/join_ff/test1/f2.csv", "w", newline="") as f2: csv.writer(f2).writerows([
+      ["amazon",    1, "shop"],
+      ["apple",     2, "application"],
+      ["facebook",  3, "ai"],
+      ["google",    4, "os"],
+      ["microsoft", 5, "cloud"],
+    ])
+    with open("./.test/join_ff/test1/f1.csv","r", newline="") as f1:
+      with open("./.test/join_ff/test1/f2.csv","r", newline="") as f2:
+        joined = csvjoin.join_ff(f1,f2)
+
+        amazon = next(joined)
+        self.assertEqual("amazon", amazon[0])
+        self.assertEqual(["amazon", "6", "cloud"], amazon[1][0][1])
+        self.assertEqual(["amazon", "1", "shop" ], amazon[1][1][1])
+
+        apple = next(joined)
+        self.assertEqual("apple", apple[0])
+        self.assertEqual(["apple", "5", "phone"       ], apple[1][0][1])
+        self.assertEqual(["apple", "2", "application" ], apple[1][1][1])
+
+        facebook = next(joined)
+        self.assertEqual("facebook", facebook[0])
+        self.assertEqual(["facebook", "8", "sns" ], facebook[1][0][1])
+        self.assertEqual(["facebook", "3", "ai"  ], facebook[1][1][1])
+
+        google = next(joined)
+        self.assertEqual("google", google[0])
+        self.assertEqual(["google", "6", "search" ], google[1][0][1])
+        self.assertEqual(["google", "4", "os"     ], google[1][1][1])
+
+        microsoft = next(joined)
+        self.assertEqual("microsoft", microsoft[0])
+        self.assertEqual(["microsoft", "9", "os"    ], microsoft[1][0][1])
+        self.assertEqual(["microsoft", "5", "cloud" ], microsoft[1][1][1])
+      pass
+    pass
   pass
 
 def main(): return unittest.main()
